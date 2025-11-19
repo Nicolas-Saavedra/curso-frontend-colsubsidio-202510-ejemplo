25cbdea52b7068cf0ecdc423d3e0a2dd1a6d2b97
Nicolas Saavedra <personal@nicolassaavedra.com>
2025-11-18 19:08:15 -0500

    feat: initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-row items-center justify-center h-screen">
+        <h1 className="text-6xl">Hola a todos desde Vercel!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
