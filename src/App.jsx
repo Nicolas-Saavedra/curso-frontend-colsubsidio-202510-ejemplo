f73333ae7b1d9c18e1d72e81be14a01c4b36c967
Nicolas Saavedra <personal@nicolassaavedra.com>
2025-11-18 19:24:58 -0500

    feat!: added new line of text

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ function App() {
   return (
     <>
       <div className="flex flex-row items-center justify-center h-screen">
-        <h1 className="text-6xl">Hola a todos desde Vercel!</h1>
+        <div>
+          <h1 className="text-6xl">Hola a todos desde Vercel!</h1>
+          <br />
+          <h2 className="text-4xl text-center">Este texto no estaba aqui antes!</h2>
+        </div>
       </div>
     </>
   );
